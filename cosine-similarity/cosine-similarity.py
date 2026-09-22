f7d3a0cdfbc1ec6e83c893f41f5ebf3d99cd46bc
Jayden Ramirez <jaydenisiahramirez@gmail.com>
2026-09-22 01:49:12 -0400

    Solve cosine-similarity

---
+++ cosine-similarity/cosine-similarity.py
@@ -0,0 +1,28 @@
+import numpy as np
+import math
+
+def cosine_similarity(a: list, b: list) -> float:
+    """
+    Returns the cosine similarity as a Python float.
+    """
+    # Write code here
+    dot_prod = 0
+    for i, v in enumerate(a):
+        dot_prod += v * b[i]
+
+    norm_a, norm_b = 0, 0
+
+    for i in a:
+        norm_a += i ** 2
+    norm_a = math.sqrt(norm_a)
+
+    for i in b:
+        norm_b += i ** 2
+    norm_b = math.sqrt(norm_b)
+
+    if norm_b == 0 or norm_a == 0:
+        return 0.0
+
+    res = dot_prod / (norm_a * norm_b)
+    
+    
