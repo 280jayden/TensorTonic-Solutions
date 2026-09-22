7c3e8aa0d29a66c91484997ba9eb21215c58d69d
Jayden Ramirez <jaydenisiahramirez@gmail.com>
2026-09-22 19:44:52 -0400

    Solve la-outer-product

--- study-plans/math-linear-algebra/la-outer-product/la-outer-product.py
+++ study-plans/math-linear-algebra/la-outer-product/la-outer-product.py
@@ -5,7 +5,7 @@ def outer_product(u: list, v: list) -> np.ndarray:
     Returns the float64 outer-product matrix.
     """
 
-    res = np.ones((len(u), len(v)))
+    res = np.zeros((len(u), len(v)))
 
     for i, value1 in enumerate(u):
         for j, value2 in enumerate(v):
